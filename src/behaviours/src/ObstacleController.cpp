46af1a10391832077a84d5803873a1877c86962d
Mason U'Ren <mason.uren600@myci.csuci.edu>
2018-02-06 17:55:19 -0800

    Simplying detection alg and trying to streamline angle calc into case statements

--- src/behaviours/src/ObstacleController.cpp
+++ src/behaviours/src/ObstacleController.cpp
@@ -1,52 +1,55 @@
 #include "ObstacleController.h"
 
-ObstacleController::ObstacleController()
-{
-  obstacleAvoided = true;
-  obstacleDetected = false;
-  obstacleInterrupt = false;
-  result.PIDMode = CONST_PID; //use the const PID to turn at a constant speed
+ObstacleController::ObstacleController() {
+    obstacleAvoided = true;
+    obstacleDetected = false;
+    obstacleInterrupt = false;
+    result.PIDMode = CONST_PID; //use the const PID to turn at a constant speed
+    detection.good_detection = false;
+    detection.last_detection = -1;
+    detection.corr_angle = 0;
+    detection.type = NO_OBSTACLE;
 }
 
 
 //note, not a full reset as this could cause a bad state
 //resets the interupt and knowledge of an obstacle or obstacle avoidance only.
 void ObstacleController::Reset() {
-  obstacleAvoided = true;
-  obstacleDetected = false;
-  obstacleInterrupt = false;
-  delay = current_time;
+    obstacleAvoided = true;
+    obstacleDetected = false;
+    obstacleInterrupt = false;
+    delay = current_time;
 }
 
 // Avoid crashing into objects detected by the ultraound
 void ObstacleController::avoidObstacle() {
-  
+
     //always turn left to avoid obstacles
     if (right < 0.8 || center < 0.8 || left < 0.8) {
-      result.type = precisionDriving;
+        result.type = precisionDriving;
 
-      result.pd.cmdAngular = -K_angular;
+        result.pd.cmdAngular = -K_angular;
 
-      result.pd.setPointVel = 0.0;
-      result.pd.cmdVel = 0.0;
-      result.pd.setPointYaw = 0;
+        result.pd.setPointVel = 0.0;
+        result.pd.cmdVel = 0.0;
+        result.pd.setPointYaw = 0;
     }
 }
 
 // A collection zone was seen in front of the rover and we are not carrying a target
 // so avoid running over the collection zone and possibly pushing cubes out.
 void ObstacleController::avoidCollectionZone() {
-  
+
     result.type = precisionDriving;
 
     result.pd.cmdVel = 0.0;
 
     // Decide which side of the rover sees the most april tags and turn away
     // from that side
-    if(count_left_collection_zone_tags < count_right_collection_zone_tags) {
-      result.pd.cmdAngular = K_angular;
+    if (count_left_collection_zone_tags < count_right_collection_zone_tags) {
+        result.pd.cmdAngular = K_angular;
     } else {
-      result.pd.cmdAngular = -K_angular;
+        result.pd.cmdAngular = -K_angular;
     }
 
     result.pd.setPointVel = 0.0;
@@ -57,128 +60,141 @@ void ObstacleController::avoidCollectionZone() {
 
 Result ObstacleController::DoWork() {
 
-  clearWaypoints = true;
-  set_waypoint = true;
-  result.PIDMode = CONST_PID;
-  string msg;
+    clearWaypoints = true;
+    set_waypoint = true;
+    result.PIDMode = CONST_PID;
+    string msg;
 
-  if (obstacleDetected) {
-    /*
-     * Update "/logger" publisher -> Starting avoidance
-     */
-    if (!logInit) {
-      msg = "Starting obstacle avoidance routine.";
-      logMessage(current_time, "ObstacleController", msg);
-      logInit = true;
+    if (obstacleDetected) {
+        /*
+         * Update "/logger" publisher -> Starting avoidance
+         */
+        if (!logInit) {
+            msg = "Starting obstacle avoidance routine.";
+            logMessage(current_time, "ObstacleController", msg);
+            logInit = true;
+        }
+
+        // The obstacle is an april tag marking the collection zone
+        if (collection_zone_seen) {
+            avoidCollectionZone();
+        } else {
+            avoidObstacle();
+        }
     }
 
-    // The obstacle is an april tag marking the collection zone
-    if(collection_zone_seen){
-      avoidCollectionZone();
+
+    //if an obstacle has been avoided
+    if (can_set_waypoint) {
+        /*
+         * Update "/logger" publisher -> Exiting avoidance
+         */
+        msg = "Exiting obstacle avoidance.";
+        logMessage(current_time, "ObstacleController", msg);
+        logInit = false;
+
+        can_set_waypoint = false; //only one waypoint is set
+        set_waypoint = false;
+        clearWaypoints = false;
+
+        result.type = waypoint;
+        result.PIDMode = FAST_PID; //use fast pid for waypoints
+        Point forward;            //waypoint is directly ahead of current heading
+        forward.x = currentLocation.x + (0.5 * cos(currentLocation.theta));
+        forward.y = currentLocation.y + (0.5 * sin(currentLocation.theta));
+        result.wpts.waypoints.clear();
+        result.wpts.waypoints.push_back(forward);
     }
-    else {
-      avoidObstacle();
-    }
-  }
 
-
-  //if an obstacle has been avoided
-  if (can_set_waypoint) {
-    /*
-     * Update "/logger" publisher -> Exiting avoidance
-     */
-    msg = "Exiting obstacle avoidance.";
-    logMessage(current_time, "ObstacleController", msg);
-    logInit = false;
-
-    can_set_waypoint = false; //only one waypoint is set
-    set_waypoint = false;
-    clearWaypoints = false;
-
-    result.type = waypoint; 
-    result.PIDMode = FAST_PID; //use fast pid for waypoints
-    Point forward;            //waypoint is directly ahead of current heading
-    forward.x = currentLocation.x + (0.5 * cos(currentLocation.theta));
-    forward.y = currentLocation.y + (0.5 * sin(currentLocation.theta));
-    result.wpts.waypoints.clear();
-    result.wpts.waypoints.push_back(forward);
-  }
-
-  return result;
+    return result;
 }
 
 
 void ObstacleController::setSonarData(float sonarleft, float sonarcenter, float sonarright) {
-  left = sonarleft;
-  right = sonarright;
-  center = sonarcenter;
+    left = sonarleft;
+    right = sonarright;
+    center = sonarcenter;
 
-  ProcessData();
+    ProcessData();
 }
 
 void ObstacleController::setCurrentLocation(Point currentLocation) {
-  this->currentLocation = currentLocation;
+    this->currentLocation = currentLocation;
 }
 
 void ObstacleController::ProcessData() {
 
-  //timeout timer for no tag messages
-  //this is used to set collection zone seen to false beacuse
-  //there is no report of 0 tags seen
-  long int Tdifference = current_time - timeSinceTags;
-  float Td = Tdifference/1e3;
-  if (Td >= 0.5) {
-    collection_zone_seen = false;
-    phys= false;
-    if (!obstacleAvoided)
-    {
-      can_set_waypoint = true;
+    //timeout timer for no tag messages
+    //this is used to set collection zone seen to false beacuse
+    //there is no report of 0 tags seen
+    long int Tdifference = current_time - timeSinceTags;
+    float Td = Tdifference / 1e3;
+    if (Td >= 0.5) {
+        collection_zone_seen = false;
+        phys = false;
+        if (!obstacleAvoided) {
+            can_set_waypoint = true;
+        }
     }
-  }
 
-  //If we are ignoring the center sonar
-  if(ignore_center_sonar){
-    //If the center distance is longer than the reactivation threshold 
-    if(center > reactivate_center_sonar_threshold){
-      //currently do not re-enable the center sonar instead ignore it till the block is dropped off
-      //ignore_center_sonar = false; //look at sonar again beacuse center ultrasound has gone long
+    //If we are ignoring the center sonar
+    if (ignore_center_sonar) {
+        //If the center distance is longer than the reactivation threshold
+        if (center > reactivate_center_sonar_threshold) {
+            //currently do not re-enable the center sonar instead ignore it till the block is dropped off
+            //ignore_center_sonar = false; //look at sonar again beacuse center ultrasound has gone long
+        } else {
+            //set the center distance to "max" to simulated no obstacle
+            center = 3;
+        }
+    } else {
+        //this code is to protect against a held block causing a false short distance
+        //currently pointless due to above code
+        if (center < 3.0) {
+            result.wristAngle = 0.7;
+        } else {
+            result.wristAngle = -1;
+        }
     }
-    else{
-      //set the center distance to "max" to simulated no obstacle
-      center = 3;
-    }
-  }
-  else {
-    //this code is to protect against a held block causing a false short distance
-    //currently pointless due to above code
-    if (center < 3.0) {
-      result.wristAngle = 0.7;
-    }
-    else {
-      result.wristAngle = -1;
-    }
-  }
 
-  // TODO: adjust 'triggerDistance' to the current max tag idenfication range (~0.6m)
-  // TODO: better evaluate sonar data that is passed
-  //if any sonar is below the trigger distance set physical obstacle true
-  if (left < triggerDistance || right < triggerDistance || center < triggerDistance)
-  {
-    phys = true;
-    timeSinceTags = current_time;
-  }
 
-  //if physical obstacle or collection zone visible
-  if (collection_zone_seen || phys)
-  {
-    obstacleDetected = true;
-    obstacleAvoided = false;
-    can_set_waypoint = false;
-  }
-  else
-  {
-    obstacleAvoided = true;
-  }
+    // TODO: CURRENT WORK
+    bool sonar_left_flag;
+    bool sonar_center_flag;
+    bool sonar_right_flag;
+
+    /*
+     * Monitor the detection values that fall below our 'MAX_THRESH'
+     */
+    if (left <= MAX_THRESH) {
+        sonar_left_flag = sonarStruct(sonar_left, left);
+    }
+    if (center <= MAX_THRESH) {
+        sonar_center_flag = sonarStruct(sonar_center, center);
+    }
+    if (right <= MAX_THRESH) {
+        sonar_right_flag = sonarStruct(sonar_right, right);
+    }
+    if ()
+
+
+    // TODO: adjust 'triggerDistance' to the current max tag idenfication range (~0.6m)
+    // TODO: better evaluate sonar data that is passed
+    //if any sonar is below the trigger distance set physical obstacle true
+    if (left < triggerDistance || right < triggerDistance || center < triggerDistance) {
+        phys = true;
+        timeSinceTags = current_time;
+    }
+
+    //if physical obstacle or collection zone visible
+    // TODO: this is where a detection is flagged and passed to either 'avoidCollectionZone' or 'avoidObstacle'
+    if (collection_zone_seen || phys) {
+        obstacleDetected = true;
+        obstacleAvoided = false;
+        can_set_waypoint = false;
+    } else {
+        obstacleAvoided = true;
+    }
 }
 
 // Report April tags seen by the rovers camera so it can avoid
@@ -186,116 +202,114 @@ void ObstacleController::ProcessData() {
 // Added relative pose information so we know whether the
 // top of the AprilTag is pointing towards the rover or away.
 // If the top of the tags are away from the rover then treat them as obstacles. 
-void ObstacleController::setTagData(vector<Tag> tags){
-  collection_zone_seen = false;
-  count_left_collection_zone_tags = 0;
-  count_right_collection_zone_tags = 0;
+void ObstacleController::setTagData(vector<Tag> tags) {
+    collection_zone_seen = false;
+    count_left_collection_zone_tags = 0;
+    count_right_collection_zone_tags = 0;
 
-  // this loop is to get the number of center tags
-  if (!targetHeld) {
-    for (int i = 0; i < tags.size(); i++) { //redundant for loop
-      if (tags[i].getID() == 256) {
+    // this loop is to get the number of center tags
+    if (!targetHeld) {
+        for (int i = 0; i < tags.size(); i++) { //redundant for loop
+            if (tags[i].getID() == 256) {
 
-	collection_zone_seen = checkForCollectionZoneTags( tags );
-        timeSinceTags = current_time;
-      }
+                collection_zone_seen = checkForCollectionZoneTags(tags);
+                timeSinceTags = current_time;
+            }
+        }
     }
-  }
 }
 
-bool ObstacleController::checkForCollectionZoneTags( vector<Tag> tags ) {
+bool ObstacleController::checkForCollectionZoneTags(vector<Tag> tags) {
 
-  for ( auto & tag : tags ) { 
+    for (auto &tag : tags) {
 
-    // Check the orientation of the tag. If we are outside the collection zone the yaw will be positive so treat the collection zone as an obstacle. 
-    //If the yaw is negative the robot is inside the collection zone and the boundary should not be treated as an obstacle. 
-    //This allows the robot to leave the collection zone after dropping off a target.
-    if ( tag.calcYaw() > 0 ) 
-      {
-	// checks if tag is on the right or left side of the image
-	if (tag.getPositionX() + camera_offset_correction > 0) {
-	  count_right_collection_zone_tags++;
-	  
-	} else {
-	  count_left_collection_zone_tags++;
-	}
-      }
-    
-  }
+        // Check the orientation of the tag. If we are outside the collection zone the yaw will be positive so treat the collection zone as an obstacle.
+        //If the yaw is negative the robot is inside the collection zone and the boundary should not be treated as an obstacle.
+        //This allows the robot to leave the collection zone after dropping off a target.
+        if (tag.calcYaw() > 0) {
+            // checks if tag is on the right or left side of the image
+            if (tag.getPositionX() + camera_offset_correction > 0) {
+                count_right_collection_zone_tags++;
+
+            } else {
+                count_left_collection_zone_tags++;
+            }
+        }
+
+    }
 
 
-  // Did any tags indicate that the robot is inside the collection zone?
-  return count_left_collection_zone_tags + count_right_collection_zone_tags > 0;
+    // Did any tags indicate that the robot is inside the collection zone?
+    return count_left_collection_zone_tags + count_right_collection_zone_tags > 0;
 
 }
 
 //obstacle controller should inrerupt is based upon the transition from not seeing and obstacle to seeing an obstacle
 bool ObstacleController::ShouldInterrupt() {
 
-  //if we see and obstacle and havent thrown an interrupt yet
-  if(obstacleDetected && !obstacleInterrupt)
-  {
-    obstacleInterrupt = true;
-    return true;
-  }
-  else
-  {
-    //if the obstacle has been avoided and we had previously detected one interrupt to change to waypoints
-    if(obstacleAvoided && obstacleDetected)
-    {
-      Reset();
-      return true;
+    //if we see and obstacle and havent thrown an interrupt yet
+    if (obstacleDetected && !obstacleInterrupt) {
+        obstacleInterrupt = true;
+        return true;
     } else {
-      return false;
+        //if the obstacle has been avoided and we had previously detected one interrupt to change to waypoints
+        if (obstacleAvoided && obstacleDetected) {
+            Reset();
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 }
 
 bool ObstacleController::HasWork() {
-  //there is work if a waypoint needs to be set or the obstacle hasnt been avoided
-  if (can_set_waypoint && set_waypoint)
-  {
-    return true;
-  }
+    //there is work if a waypoint needs to be set or the obstacle hasnt been avoided
+    if (can_set_waypoint && set_waypoint) {
+        return true;
+    }
 
-  return !obstacleAvoided;
+    return !obstacleAvoided;
 }
 
 //ignore center ultrasound
-void ObstacleController::setIgnoreCenterSonar(){
-  ignore_center_sonar = true; 
+void ObstacleController::setIgnoreCenterSonar() {
+    ignore_center_sonar = true;
 }
 
-void ObstacleController::setCurrentTimeInMilliSecs( long int time )
-{
-  current_time = time;
+void ObstacleController::setCurrentTimeInMilliSecs(long int time) {
+    current_time = time;
 }
 
 void ObstacleController::setTargetHeld() {
-  targetHeld = true;
+    targetHeld = true;
 
-  //adjust current state on transition from no cube held to cube held
-  if (previousTargetState == false) {
-    obstacleAvoided = true;
-    obstacleInterrupt = false;
-    obstacleDetected = false;
-    previousTargetState = true;
-  }
+    //adjust current state on transition from no cube held to cube held
+    if (previousTargetState == false) {
+        obstacleAvoided = true;
+        obstacleInterrupt = false;
+        obstacleDetected = false;
+        previousTargetState = true;
+    }
 }
 
-void ObstacleController::setTargetHeldClear()
-{
-  //adjust current state on transition from cube held to cube not held
-  if (targetHeld)
-  {
-    Reset();
-    targetHeld = false;
-    previousTargetState = false;
-    ignore_center_sonar = false;
-  }
+void ObstacleController::setTargetHeldClear() {
+    //adjust current state on transition from cube held to cube not held
+    if (targetHeld) {
+        Reset();
+        targetHeld = false;
+        previousTargetState = false;
+        ignore_center_sonar = false;
+    }
 }
 
-// TODO: build 'vectorIterator' function
+//TODO: implement correction angle calculator based on acceptable values
+void ObstacleController::correctionAngle() {
+    /*
+     *
+     */
+}
+
+
 /**
  * Iterates of the passed structure and verifies detection are of acceptable range.
  * Note: Acceptable values are messured against 'DELTA' (calculated max dist. the rover can cover in 1 sec.)
@@ -303,49 +317,42 @@ void ObstacleController::setTargetHeldClear()
  * @param buffer : referenced structure (vector)
  * @return detection : a d
  */
-OBJ_DETECTION ObstacleController::sonarAnalysis(std::vector<float> &buffer) {
-    OBJ_DETECTION detection;
+void ObstacleController::sonarAnalysis(std::vector<float> &buffer) {
     float prev;
     float curr;
     bool has_begun = false;
+    bool bad_detection = false;
     // check to make sure the passed structure has been populated
-    if (buffer.empty()) {
-        detection.good_detection = false;
-    }
-    else {
+    if (!buffer.empty()) {
         for (auto detectionRange : buffer) {
             if (!has_begun) {
                 curr = detectionRange;
                 has_begun = true;
-            }
-            else {
+            } else {
                 prev = curr;
                 curr = detectionRange;
                 double diff = std::fabs(prev - curr);
                 if (diff < DELTA) {
                     detection.last_detection = curr;
                     detection.good_detection = true;
-//                    last_detect = curr;
+    //                    last_detect = curr;
                     // TESTING: noticed false detections
-//                    if (curr > 0.06) {
-//                        last_detect = curr;
-//                    }
-//                    else {
-//                        bad_detection = true;
-//                        last_detect = -1;
-//                    }
-                }
-                else { // Throw out bad vector
+    //                    if (curr > 0.06) {
+    //                        last_detect = curr;
+    //                    }
+    //                    else {
+    //                        bad_detection = true;
+    //                        last_detect = -1;
+    //                    }
+                } else { // Throw out bad vector
                     detection.good_detection = false;
+                    break;
                 }
             }
         }
     }
-    return detection;
 }
 
-
-// TODO: build 'vectorManager' function
 /**
  * Adds passed values of sonar detections to respective structures, then checks
  * to make sure structure doesn't exceed specified size 'VECTOR_MAX'
