7010e6ae69bc620398a7f3262c76eb994463f092
Ethan Warner <ethan.warner975@myci.csuci.edu>
2018-02-08 15:35:15 -0800

    SWAR-24-2
    Just the added files and changed cmakelist.txt for the Utilities.cpp file

---
+++ src/behaviours/src/Utilities.h
@@ -0,0 +1,22 @@
+//Ethan Warner
+
+#ifndef UTILITIES
+#define UTILITIES
+
+#include "Point.h"
+
+class Utilities {
+
+public:
+    static float distance_between_points(Point, Point);
+
+    static float angle_between_points(Point, Point);
+
+    static float difference_between_angles(Point, Point);
+
+    static int saturation_check_left(int, int);
+
+    static int saturation_check_right(int, int);
+};
+
+#endif
